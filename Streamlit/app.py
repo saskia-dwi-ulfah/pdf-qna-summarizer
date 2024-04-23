004aa1af30cb2abe831fecc8ffababcdc86df9a1
Victor Nugraha <victor.nugraha37@gmail.com>
2024-04-23 17:35:38 +0700

    Add files via upload
    add cache function

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -17,7 +17,6 @@ from langchain_core.runnables import RunnablePassthrough
 import os
 import shutil
 import tempfile
-import streamlit as st
 
 from utils.function import format_docs, display_how_to
 from utils.template import template
@@ -31,11 +30,19 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain.sentence_transformers import SentenceTransformer
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from dotenv import load_dotenv
 from langchain import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
+
+# @st.cache(allow_output_mutation=True)
+# def cached_function(input_list):
+#     # Modify the input list
+#     input_list.append()
+#     return input_list
 
 # Add a title to the sidebar
 with st.sidebar:
@@ -48,7 +55,7 @@ with st.sidebar:
     
     option = st.sidebar.selectbox(
         'Choose Menu',
-        ('Home', 'QnA', 'Summarizer')
+        ('QnA', 'Summarizer')
     )
 
     st.sidebar.markdown('---')
@@ -60,15 +67,13 @@ with st.sidebar:
     uploaded_file = st.file_uploader("Upload your PDF file", type = ["pdf"])
 
 # Display content based on the selected option
-if option == 'Home':
-    st.title('Home Page')
-    st.write('Welcome to the Home Page!')
-
-elif option == 'QnA':
+    
+if option == 'QnA':
     st.title("Ask The PDF 📑🔮🤔")
     st.caption("Powered by Open AI GPT 3.5 Turbo")
 
     if uploaded_file is not None: 
+        
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
             # Copy the uploaded file to the temporary file
             shutil.copyfileobj(uploaded_file, tmpfile)
@@ -123,7 +128,8 @@ elif option == 'QnA':
 elif option == 'Summarizer':
     st.title("Summarize The PDF 📑🔮🤔")
     st.caption("Powered by Open AI GPT 3.5 Turbo")
-
+    
+    
     if uploaded_file is not None: 
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
             # Copy the uploaded file to the temporary file
@@ -133,26 +139,37 @@ elif option == 'Summarizer':
         loader = PyPDFLoader(tmpfile_path, extract_images = False) # error when load rapidocr-onnxruntime
         docs = loader.load()
 
-        text_splitter = CharacterTextSplitter(separator = "\n", 
-                                        chunk_size = 1000, 
-                                        chunk_overlap = 10, 
-                                        length_function = len)
-        text = text_splitter.split_documents(docs)
+        @st.cache_data
+        def coba():
+            text_splitter = CharacterTextSplitter(separator = "\n", 
+                                            chunk_size = 1000, 
+                                            chunk_overlap = 10, 
+                                            length_function = len)
+            text = text_splitter.split_documents(docs)
 
-        embedding_function = SentenceTransformerEmbeddings(model_name = "all-MiniLM-L6-v2")
+            embedding_function = SentenceTransformerEmbeddings(model_name = "all-MiniLM-L6-v2")
 
-        vectordb = Chroma.from_documents(documents = docs, embedding = OpenAIEmbeddings(openai_api_key = api_key))
+            vectordb = Chroma.from_documents(documents = docs, embedding = OpenAIEmbeddings(openai_api_key = api_key))
 
-        retriever = vectordb.as_retriever()
-        llm = OpenAI(temperature = 0.9, openai_api_key = api_key)
+            retriever = vectordb.as_retriever()
+            llm = OpenAI(temperature = 0.9, openai_api_key = api_key)
 
-        qa_chain = RetrievalQA.from_chain_type(llm = llm,
-                                            chain_type = "stuff",
-                                            retriever = retriever,
-                                            return_source_documents = True,
-                                            verbose = False)
+            qa_chain = RetrievalQA.from_chain_type(llm = llm,
+                                                chain_type = "stuff",
+                                                retriever = retriever,
+                                                return_source_documents = True,
+                                                verbose = False)
 
-        chain_result = qa_chain("Can you give me the summary")
-        answer = chain_result["result"]
-        st.write(answer)
+            chain_result = qa_chain("Can you give me the summary")
+            answer = chain_result["result"]
+
+            # cached_result = cached_function(answer)
+
+            st.write(answer)
+        coba()
+
+    else:
+        st.write("Please upload a file to summarize.")
+
+    
 
